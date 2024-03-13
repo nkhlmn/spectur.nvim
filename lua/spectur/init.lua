a6b1a6954bf9fdc1468f593d83d6655cdd3f5b20
Nikhil <nikhil.kamineni@gmail.com>
2024-03-12 20:31:43 -0700

    add format/lsp config files

--- lua/spectur/init.lua
+++ lua/spectur/init.lua
@@ -5,9 +5,9 @@ local view = require('spectur.view')
 ---@field output 'tab' | 'float' | 'notify' | 'cursor'
 ---@field view_format 'toml' | 'json
 local default_opts = {
-  notify = true,
-  output = 'float',
-  view_format = 'toml'
+	notify = true,
+	output = 'float',
+	view_format = 'toml',
 }
 
 ---@class (exact) Spectur
@@ -15,26 +15,26 @@ local default_opts = {
 ---@field display fun(url: string, opts?: Opts)
 ---@field setup fun(opts: Opts)
 local M = {
-  opts = default_opts,
+	opts = default_opts,
 }
 
 ---display the parsed url specified by the opts
 ---@param url string
 ---@param opts Opts|nil
 function M.display(url, opts)
-  if opts == nil then
-    opts = M.opts
-  end
+	if opts == nil then
+		opts = M.opts
+	end
 
-  view.handlers[opts.output](url, opts)
+	view.handlers[opts.output](url, opts)
 
-  if opts.notify then
-    vim.notify('URL parsed')
-  end
+	if opts.notify then
+		vim.notify('URL parsed')
+	end
 end
 
 function M.setup(opts)
-  M.opts = vim.tbl_deep_extend('force', M.opts, opts or {})
+	M.opts = vim.tbl_deep_extend('force', M.opts, opts or {})
 end
 
 return M
